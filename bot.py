d2f4f126025ab192d29cb0d74c2406cb8c36744e
mdnur8665271-cmd <mdnur8665271@gmail.com>
2026-07-21 21:18:56 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
@@ -12,6 +12,24 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer("🎉 Welcome to Daily Earning Bot!")
+
+@dp.message(Command("ads"))
+async def ads(message: Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🎥 Watch Ad",
+                    url="https://omg10.com/4/11368110"
+                )
+            ]
+        ]
+    )
+
+    await message.answer(
+        "💰 Reward পেতে নিচের বাটনে ক্লিক করুন।",
+        reply_markup=keyboard
+    )
 
 async def main():
     await dp.start_polling(bot)
